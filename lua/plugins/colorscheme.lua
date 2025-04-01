23e3ffee4156f0852cb0aaa3180c85353aa95073
Ryan Rolón <ryan.rolon@upr.edu>
2025-04-01 13:57:53 -0400

    Updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,11 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	opts = {},
 	config = function()
-		require("tokyonight").setup({
-			transparent = true,
-			vim.cmd.colorscheme("tokyonight-storm"),
+		require("catppuccin").setup({
+			transparent_background = true,
+			vim.cmd.colorscheme("catppuccin-macchiato"),
 		})
 	end,
 }
